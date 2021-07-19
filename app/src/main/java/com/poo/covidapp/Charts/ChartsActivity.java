cd89ee54f00f35d804b70335c59f1ed936069983
Matheus <matheus.crqra@gmail.com>
2021-07-19 10:09:59 -0300

    Add Charts Activity custom title, back button and implement binding

--- app/src/main/java/com/poo/covidapp/Charts/ChartsActivity.java
+++ app/src/main/java/com/poo/covidapp/Charts/ChartsActivity.java
@@ -1,32 +1,57 @@
 package com.poo.covidapp.Charts;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.poo.covidapp.R;
+import com.poo.covidapp.databinding.ActivityChartsBinding;
 
 import java.util.ArrayList;
 
 public class ChartsActivity extends AppCompatActivity {
-    private BarChart chart;
+    private ActivityChartsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charts);
+        binding = ActivityChartsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
 
-        chart = findViewById(R.id.bar_chart);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        ArrayList<String> initials = (ArrayList<String>)getIntent().getSerializableExtra("initials");
+
+        // chart = findViewById(R.id.bar_chart) == binding.barChart
+
+        ArrayList<String> initials = (ArrayList<String>) getIntent().getSerializableExtra("initials");
         for (String initials_ : initials) {
             System.out.println(initials_);
         }
 
-        ArrayList<Float> values = (ArrayList<Float>)getIntent().getSerializableExtra("values");
+        ArrayList<Float> values = (ArrayList<Float>) getIntent().getSerializableExtra("values");
         for (float value : values) {
             System.out.println(value);
         }
+
+        // Show back button and set custom title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Batata");
+    }
+
+    @Override
+    // Perform "back"
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) onBackPressed();
+        return super.onOptionsItemSelected(item);
     }
 }
